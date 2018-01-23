b68109b07e9d95f3c0e12928951c6412e37b03bf
cjwsstrm <cjwesstrom@gmail.com>
2018-01-23 11:31:16 -0800

    added a few console logs to track what happens during the recursion

--- recursion-summing-example.js
+++ recursion-summing-example.js
@@ -15,8 +15,11 @@ console.log('Loop:', sumToOneL(4));
 // Recursive case is n > 1
 function sumToOneR(n) {
   if (n === 1) {
+    console.log(`Base case, n currently is: ${n} \n`)
     return 1;
   }
+  console.log(`Where n = ${n}, n + sumtToOneR(n - 1) is: ${n + sumToOneR(n - 1)}`)
+  console.log(`Recursive case, n currently is: ${n} \n`)
   return n + sumToOneR(n - 1);
 }
 
