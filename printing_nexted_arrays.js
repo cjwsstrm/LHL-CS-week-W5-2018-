eb12b682d53e20684ba543f6856e06bba22f8443
cjwsstrm <cjwesstrom@gmail.com>
2018-01-24 16:18:11 -0800

    finished recursion excercises and created initial class in durian-tree.js

--- printing_nexted_arrays.js
+++ printing_nexted_arrays.js
@@ -11,3 +11,20 @@ function printItems(array) {
 
 const array = ["words", [["are", ["better"]], [[[",right?"]], "Yes"]]];
 printItems(array);
+
+
+// An example of using iteration instead. Harder to read / interpret 
+
+// function printItems(array) {
+//   while (array.length > 0) {
+//     const element = array.shift();
+
+//     if (Array.isArray(element)) {
+//       element.reverse().forEach((newElement)=> {
+//         array.unshift(newElement);
+//       });
+//     } else {
+//       console.log(element);
+//     }
+//   }
+// }
